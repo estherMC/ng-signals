95a53edb60afebb31e3c95c5fb2751ff1531e98b
Esther Martorell <esther_3@hotmail.com>
2023-08-08 12:48:05 +0200

    :sparkles: examples of set, update, mutate signals

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -8,17 +8,22 @@ import { Component, signal } from '@angular/core';
   imports: [NgFor],
 })
 export class SignalsComponent {
-  actions: string[] = [];
+  actions = signal<string[]>([]);
   counter = signal(0);
 
   increment() {
     //Signals operations --> set, update, mutate...
-    this.counter.update((oldCounter) => oldCounter + 1);
-    this.actions.push('INCREMENT');
+    // this.counter.update((oldCounter) => oldCounter + 1);
+    //Alternatives:
+    this.counter.set(this.counter() + 1);
+    this.actions.mutate((oldActions) => oldActions.push('INCREMENT'));
+    // this.actions.push('INCREMENT');
   }
 
   decrement() {
     this.counter.update((oldCounter) => oldCounter - 1);
-    this.actions.push('DECREMENT');
+    //Same than option of increment in an alternative way:
+    this.actions.update((oldActions) => [...oldActions, 'DECREMENT']);
+    // this.actions.push('DECREMENT');
   }
 }
